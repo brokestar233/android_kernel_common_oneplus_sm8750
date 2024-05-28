b6b24b6e2796ae29fd17dba6073e631c8118c62e
wengle <wengle@oppo.com>
2024-05-28 15:02:13 +0800

    [BSP.Kernel.MM][1/1]{vendor_hooks: Add tune scan type hook in get_scan_count()}
    适用范围：{SM8750|android v|kernel6.6}
    准入id：{OPPO-7124097}
    分析：{
    kernel-commit-from-upstream:facf733510a3d1d52efc64e699e408fc7abb02cb
    }
    方案：{
    Google gerrit link:
    https://android-review.googlesource.com/c/kernel/common/+/3022264
    
    Add hook in get_scan_count() for oem to wield customized reclamation strategy
    }
    风险及影响[快/稳/省/功能/安全隐私]：{快}
    测试建议：{老化测试&整机性能}
    跨组依赖(topic name)：{无}
    
    Change-Id: Ic921d8aa666c1bc3c488f0e96d476c4f826596d4
    Reviewed-on: http://gerrit.scm.adc.com:8080/28987086
    almid-check: cqc <oppo_codecheck@oppo.com>
    Reviewed-by: 翁乐-80379123 <wengle@oppo.com>
    Reviewed-by: 谢晓宣-80398989 <xiexiaoxuan@oppo.com>
    Reviewed-by: 李杨欧文-80351790 <liyangouwen1@oppo.com>
    Reviewed-by: 周华材-80209858 <zhouhuacai@oppo.com>
    Reviewed-by: 张正琪-80386171 <zhangzhengqi@oppo.com>
    Reviewed-by: 谢柳杰-80233409 <xieliujie@oppo.com>
    rules-check: pswbuild <pswbuild@oppo.com>
    Reviewed-by: code_guard <code_guard@oppo.com>
    code-check: cqc <oppo_codecheck@oppo.com>
    Reviewed-by: pswbuild <pswbuild@oppo.com>
    Tested-by: pswbuild <pswbuild@oppo.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -305,3 +305,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_freeze_whether_wake);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_percpu_rwsem_wq_add);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_save_track_hash);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_vmscan_kswapd_done);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_tune_scan_type);
